ef4accd4e108c19796f97657b8501df24d86ccd6
Daniel Loureiro Cintra <cintra.70@gmail.com>
2017-04-22 19:37:12 -0300

    arquivos do projeto

---
+++ src/components/Index.vue
@@ -0,0 +1,34 @@
+<script>
+
+	import CcForm from './expenses/form.vue'
+	import CcList from './expenses/list.vue'
+	
+	export default{
+		components: {
+			CcForm,
+			CcList
+		}
+
+	}
+</script>
+
+<template>
+	<q-layout>
+		<div slot="header" class="toolbar">
+			<q-toolbar-title :padding="0">
+				Despesas em dinheiro vivo
+			</q-toolbar-title>
+		</div>
+		<div class="container">
+			<cc-form></cc-form>
+			<cc-list></cc-list>
+		</div>
+	</q-layout>
+</template>
+
+<style scoped>
+	.container{
+		padding: 20px;
+	}
+</style>
+
---
+++ src/components/expenses/form.vue
@@ -0,0 +1,38 @@
+<script>
+	export default{
+		data(){
+			return {
+				expense:{
+					amount: '',
+					description: '',
+					date: ''
+				}
+			}
+		},
+		methods:{
+			submit () {
+				this.$store.commit('ADD_EXPENSE',this.expenses)
+			}
+		}
+	}
+</script>
+
+<template>	
+	<form @submit.prevent="submit">
+		<input class="my-input" type="number" v-model="expense.amount" placeholder="R$">
+		<input class="my-input" type="text" v-model="expense.description" placeholder="Descrição">
+		<input class="my-input" type="text" v-model="expense.date" placeholder="Data">
+
+		<button class="primary my-button">Salvar</button>
+	</form>
+</template>
+
+<style scoped>
+	.my-input{
+		width: 100%;
+		margin-bottom: 20px; 
+	}
+	.my-button{
+		width: 100%;
+	}
+</style>
